40ec64d5e207693c6233b47c7546a0e2875489b1
Ole Martin <olemartin.skaug@gmail.com>
2015-07-12 18:55:17 +0200

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Yodel
-PHP framework
---
+++ y-core/init.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*
+* This file works as the core includer of all the classes!
+* You only need to include this file on your page to access
+* all of the Yodels framework functions. 
+* @author Ole Martin Skaug @ Yodel
+* @version 1.0 - strawberry
+*
+*/
+
+?>
